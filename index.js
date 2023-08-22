2f591aba6a5872abcd9e826ae5ffef431ed8b15a
Dawit Tefera <69167983+tevBoyz@users.noreply.github.com>
2023-08-22 12:36:36 +0300

    Add files via upload
    commit

---
+++ index.js
@@ -0,0 +1,110 @@
+var buffer = '';
+var memory = '';
+
+var myScreen = document.getElementById("screen");
+var secScreen = document.getElementById("equation");
+
+
+function updatedisp(){
+    myScreen.value = buffer;
+    console.log(buffer);
+    secScreen.innerHTML = memory;
+}
+
+function clearall(){
+    buffer = '';
+    memory = '';
+    updatedisp();
+}
+
+function percentage(){
+    buffer += ('%');
+    updatedisp();
+}
+
+function divide(){
+    buffer += ('/');
+    updatedisp();
+}
+
+function multiply(){
+    buffer += ('*');
+    updatedisp();
+}
+
+function subtract(){
+    buffer += ('-');
+    updatedisp();
+}
+
+function add(){
+    buffer += ('+');
+    updatedisp();
+}
+
+function equals(){
+    // buffer += ('=');
+    memory = buffer + " equals";
+    buffer = "" + eval(buffer) +"";
+    updatedisp();
+}
+
+function backspace(){
+    buffer.pop;
+    updatedisp();
+}
+
+function period(){
+    buffer += ('.');
+    updatedisp();
+}
+
+function seven(){
+    buffer += ('7');
+    updatedisp();
+}
+
+function eight(){
+    buffer += ('8');
+    updatedisp();
+}
+
+function nine(){
+    buffer += ('9');
+    updatedisp();
+}
+
+function six(){
+    buffer += ('6');
+    updatedisp();
+}
+
+function five(){
+    buffer += ('5');
+    updatedisp();
+}
+
+function four(){
+    buffer += ('4');
+    updatedisp();
+}
+
+function three(){
+    buffer += ('3');
+    updatedisp();
+}
+
+function two(){
+    buffer += ('2');
+    updatedisp();
+}
+
+function one(){
+    buffer += ('1');
+    updatedisp();
+}
+
+function zero(){
+    buffer += ('0');
+    updatedisp();
+}
